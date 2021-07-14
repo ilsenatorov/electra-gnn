dbd7f9e6d1eb117b296753842e96d321c0adbf19
Ilya Senatorov <il.senatorov@protonmail.com>
2021-07-14 17:16:36 +0200

    custom optimisation

--- electra_gnn/models/pretraining_model.py
+++ electra_gnn/models/pretraining_model.py
@@ -17,26 +17,33 @@ class PretrainingModel(LightningModule):
         self.discriminator = Discriminator(feat_dim)
         self.gen_loss = torch.nn.BCEWithLogitsLoss(reduction='sum')
         self.disc_loss = torch.nn.BCEWithLogitsLoss(reduction='sum')
+        self.automatic_optimization = False
 
-    def forward(self, batch):
-        # create a masked copy of the original data
+    def generate(self, batch):
+        masked_batch = mask_molecules(batch, mask_ratio=self.hparams.mask_ratio)
+        generated_features = self.generator(masked_batch)  # Try to predict masked features from the original data
+        return generated_features
+
+    def training_step(self, batch, batch_idx):
+        genoptim, discoptim = self.optimizers()
         masked_batch = mask_molecules(batch, mask_ratio=self.hparams.mask_ratio)
         masked_idx = masked_batch.masked_idx
         generated_features = self.generator(masked_batch)  # Try to predict masked features from the original data
         gen_loss = self.gen_loss(batch.x[masked_idx], generated_features)  # calculate loss
+        genoptim.zero_grad()
+        self.manual_backward(gen_loss)
+        genoptim.step()
+
         sigmoid_generated_features = torch.sigmoid(generated_features).detach().round()
         corrupt_batch = corrupt_molecules(batch, sigmoid_generated_features, masked_idx)
         disc_prediction = self.discriminator(corrupt_batch)
         corruption_labels = torch.zeros((batch.num_nodes, 1), dtype=torch.float32, device=self.device)
         corruption_labels[masked_idx] = 1.
         disc_loss = self.disc_loss(corruption_labels, disc_prediction)
-        return gen_loss, disc_loss
-
-    def training_step(self, batch, batch_idx, optimizer_idx):
-        gen_loss, disc_loss = self.forward(batch)
-        return {'loss' : gen_loss + disc_loss}
-    
-
+        discoptim.zero_grad()
+        self.manual_backward(disc_loss)
+        discoptim.step()
+        self.log_dict({'gen_loss': gen_loss, 'disc_loss': disc_loss, 'loss': gen_loss+disc_loss})
 
     def configure_optimizers(self):
         genoptim = Adam(self.generator.parameters())
--- train.py
+++ train.py
@@ -1,8 +1,27 @@
 from pytorch_lightning import Trainer
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 from torch_geometric.data import DataLoader
 
 from electra_gnn.models.pretraining_model import PretrainingModel
 from electra_gnn.utils.data import MoleculeDataset
+
+logger = TensorBoardLogger('tb_logs',
+                           name='electra',
+                           default_hp_metric=False)
+callbacks = [
+    ModelCheckpoint(monitor='loss',
+                    save_top_k=3,
+                    mode='min'),
+    EarlyStopping(monitor='loss',
+                  patience=10,
+                  mode='min')
+]
+trainer = Trainer(gpus=1,
+                  callbacks=callbacks,
+                  logger=logger,
+                  )
+
 
 dataset = MoleculeDataset('raw_data/chembl_smiles.csv')
 model = PretrainingModel()
